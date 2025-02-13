dd876547e615e32eed047780befacb603198eb51
Risqi Romadhoni <risqi.romadhoni96@gmail.com>
2025-02-13 11:12:12 +0700

    fix: style of shoping card

---
+++ src/assets/plants/index.js
@@ -0,0 +1,12 @@
+export const PLANTS_IMG = {
+  monstrea: new URL("./monstrea.png", import.meta.url).href,
+  snake: new URL("./snake.png", import.meta.url).href,
+  fiddle: new URL("./fiddle.png", import.meta.url).href,
+  pothos: new URL("./pothos.png", import.meta.url).href,
+  zz: new URL("./zz.png", import.meta.url).href,
+  rubber: new URL("./rubber.png", import.meta.url).href,
+  aloe: new URL("./aloe.png", import.meta.url).href,
+  lily: new URL("./lily.png", import.meta.url).href,
+  spider: new URL("./spider.png", import.meta.url).href,
+  bird: new URL("./bird.png", import.meta.url).href,
+}
